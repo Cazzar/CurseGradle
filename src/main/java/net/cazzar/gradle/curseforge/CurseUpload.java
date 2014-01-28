f3280743343008be07cd879ca80f352507e8c203
Cayde Dixon <cayde.dixon@gmail.com>
2014-01-28 14:36:45 +1100

    Fix up copyright year

--- src/main/java/net/cazzar/gradle/curseforge/CurseUpload.java
+++ src/main/java/net/cazzar/gradle/curseforge/CurseUpload.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) ${year} Cayde Dixon (Cazzar)
+ * Copyright (C) 2014 Cayde Dixon (Cazzar)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
